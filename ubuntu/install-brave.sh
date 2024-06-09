d2ee3af0f8b7d154018e2de9795133326a0030f6
Tu Nguyen Do <tunguyendo135ndt@gmail.com>
2024-06-09 11:09:49 +0700

    reorganization

---
+++ ubuntu/install-brave.sh
@@ -0,0 +1,9 @@
+ apt install curl
+
+ curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"| tee /etc/apt/sources.list.d/brave-browser-release.list
+
+ apt update
+
+ apt install brave-browser
